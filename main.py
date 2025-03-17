7575f806f4781f6828ddf2a71ca52838743f6fb2
Zakariaelfettahi <zakariaelfettahi@gmail.com>
2025-03-16 22:32:16 -0400

    hid the token

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import os
 import discord
+
+token = os.getenv('DISCORD_BOT_TOKEN')
 
 class Client(discord.Client):
     async def on_ready(self):
-        print(f"{self.user} connected succsefully")
+        print(f"{self.user} connected successfully")
     
-    async def on_message(self,message):
-        print(f'message from {message.author}: {message.content}')
+    async def on_message(self, message):
+        if message.author == self.user:
+            return  # Prevents an infinite loop of bot replying to itself
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run('MTM1MTAwODk1MTQ5MTIzMTg1Nw.G5SyK3.A7BaKgWz_145M7IKA312pgmrArAQXBNZJwdzZ4')
+client.run(token)
