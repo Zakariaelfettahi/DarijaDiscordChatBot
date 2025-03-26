070a27440506f81599429f64c12d181ab4a3400d
Zakariaelfettahi <zakariaelfettahi@gmail.com>
2025-03-26 10:06:38 -0400

    added triviva command

--- commands_cog.py
+++ commands_cog.py
@@ -6,6 +6,8 @@ import random
 import csv
 import os
 import requests
+import html  # For decoding HTML entities
+import asyncio
 import google.generativeai as genai
 
 # Path configurations
@@ -23,12 +25,18 @@ SLURS = ['zabi', 'zebi', 'thawa', 'tqwd', 't9wd', 't7wa', 'zaml', 'li7wak', 'li 
          'sir', 'lay', 'lyn3el', 'lyn3l', 'tbonmok', 'tbon', 'qhba', '9hba', 'zeb', 'w9', 'khtek', 'zobi', '7choun', 'zab']
 FES = ["fes" , "FES" ,"Fes"]
 
+# Constants
+OTDB_API_URL = "https://opentdb.com/api.php?amount=1&type=multiple"  # Get 1 multiple-choice question
+WINNING_SCORE = 102400
+# In-memory user scores (Not persistent, will reset when the bot restarts)
+user_scores = {}
 
 class BotCommands(commands.Cog):
     def __init__(self, bot, gemini_api_key, weather_api_key):
         self.bot = bot
         self.gemini_api_key = gemini_api_key
         self.weather_api_key = weather_api_key
+        self.bot.persistent_views_added = False  # Flag to prevent the persistent view being added multiple times
 
         if gemini_api_key:
             genai.configure(api_key=gemini_api_key)
@@ -37,6 +45,67 @@ class BotCommands(commands.Cog):
             self.model = None
 
         self.BASE_URL = "https://api.weatherapi.com/v1/current.json"
+    class TriviaView(discord.ui.View):  # Renamed to TriviaView
+        def __init__(self, question_data, user_id):
+            super().__init__(timeout=None)
+            if question_data is not None:
+                self.question_data = question_data
+                self.correct_answer = html.unescape(question_data['correct_answer']) # Decode HTML entities
+                incorrect_answers = [html.unescape(answer) for answer in question_data['incorrect_answers']]
+                self.answers = [self.correct_answer] + incorrect_answers
+                random.shuffle(self.answers)
+                self.user_id = user_id
+
+                # Create buttons, assigning correct answer
+                self.a_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[0][:80], custom_id="button_a")
+                self.b_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[1][:80], custom_id="button_b")
+                self.c_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[2][:80], custom_id="button_c")
+                self.d_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[3][:80], custom_id="button_d")
+                # Add callback functions to the buttons
+                self.a_button.callback = self.create_callback(self.answers[0])
+                self.b_button.callback = self.create_callback(self.answers[1])
+                self.c_button.callback = self.create_callback(self.answers[2])
+                self.d_button.callback = self.create_callback(self.answers[3])
+
+                # Add buttons to the view
+                self.add_item(self.a_button)
+                self.add_item(self.b_button)
+                self.add_item(self.c_button)
+                self.add_item(self.d_button)
+
+        def create_callback(self, button_answer):
+            async def callback(interaction: discord.Interaction):
+                is_correct = (button_answer == self.correct_answer)
+                if is_correct:
+                    # Update Score
+                    current_score = user_scores.get(self.user_id, 0)  # Get user's score or 0
+                    if current_score == 0:
+                        new_score = 100
+                    else:
+                        new_score = current_score * 2
+
+                    user_scores[self.user_id] = new_score
+
+                    if new_score >= WINNING_SCORE:
+                        description = f"Congratulations! You've reached {WINNING_SCORE} and won the game!"
+                    else:
+                        description = f"Correct! You won {new_score} points!"
+                else:
+                    description = f"Incorrect. The correct answer was {self.correct_answer}"
+                    user_scores[self.user_id] = 0  # Reset score on wrong answer
+
+                # Get updated score
+                updated_score = user_scores.get(self.user_id, 0)
+                embed = discord.Embed(title="Trivia Result", description=f"{description}\nYour score: {updated_score}", color=discord.Color.green() if is_correct else discord.Color.red())
+                #Game Over
+                if updated_score >= WINNING_SCORE:
+                    del user_scores[self.user_id]
+                    view = None
+                else:
+                    view = self
+
+                await interaction.response.edit_message(embed=embed, view=view) # Remove buttons on result
+            return callback
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -50,8 +119,7 @@ class BotCommands(commands.Cog):
             await message.channel.send(f"Matkheserch lhdra a w9 {message.author.mention}")
 
         if any(fes in message.content.lower() for fes in FES):
-          await message.channel.send(f"Fes li 7akma l3alam {message.author.mention}")
-
+            await message.channel.send(f"Fes li 7akma l3alam {message.author.mention}")
 
     @app_commands.command(name='terjem', description='Translate Arabic to English')
     async def terjem(self, interaction: discord.Interaction, text: str):
@@ -61,7 +129,8 @@ class BotCommands(commands.Cog):
             cursor.execute("PRAGMA table_info(translations)")
             columns = [row[1] for row in cursor.fetchall() if row[1].startswith("n")]
             where_clause = " OR ".join([f"{col}=?" for col in columns]) + " OR darija_ar=? OR eng=?"
-            cursor.execute(f"SELECT eng FROM translations WHERE {where_clause}", (*[word] * len(columns), word, word))
+            cursor.execute(f"SELECT eng FROM translations WHERE {where_clause}",
+                           (*[word] * len(columns), word, word))
             result = cursor.fetchone()
             conn.close()
             return result[0] if result else None
@@ -173,7 +242,7 @@ class BotCommands(commands.Cog):
             humidity = data['current']['humidity']
             feels_like = data['current']['feelslike_c']
 
-            embed = discord.Embed(title=f"Ljew f {city.title()}", color = 0xADD8E6)
+            embed = discord.Embed(title=f"Ljew f {city.title()}", color=0xADD8E6)
             embed.add_field(name="Ki dayra d3wa", value=condition, inline=False)
             embed.add_field(name="L7arara", value=f"{temp_c}°C / {temp_f}°F", inline=False)
             embed.add_field(name="L7arara bach ghat7es", value=f"{feels_like}°C", inline=False)
@@ -187,17 +256,121 @@ class BotCommands(commands.Cog):
     @app_commands.command(name='mo3awana', description='Displays all available commands and their descriptions.')
     async def mo3awana(self, interaction: discord.Interaction):
         desc_helpme = '__**Kifach tkhdem b lbot **__\n\n' \
-        '**/nokta** = ila nghiti dhek 😂\n' \
-        '**/maqoula** = ila bghiti l7ikma 🧐\n'\
-        '**/terjem** = ila bghiti terjem mn darija l english (eg: !translate salam) 🇲🇦🇬🇧\n'\
-        '**/pileouface** = ila tlefti w ma3refti madir, pile ou face 🎲\n'\
-        '**/meme** = ila bghiti chi meme 🖼️\n'\
-        '**/ai** = ila bghiti tswl l ai (gemini), text only 🤖\n'\
-        '**/ljew** = ila bghiti t3ref ljew d chi mdina (eg: !ljew Csablanca) 🌦️\n'
+                      '**/nokta** = ila nghiti dhek 😂\n' \
+                      '**/maqoula** = ila bghiti l7ikma 🧐\n' \
+                      '**/terjem** = ila bghiti terjem mn darija l english (eg: !translate salam) 🇲🇦🇬🇧\n' \
+                      '**/pileouface** = ila tlefti w ma3refti madir, pile ou face 🎲\n' \
+                      '**/meme** = ila bghiti chi meme 🖼️\n' \
+                      '**/ai** = ila bghiti tswl l ai (gemini), text only 🤖\n' \
+                      '**/ljew** = ila bghiti t3ref ljew d chi mdina (eg: !ljew Csablanca) 🌦️\n'
 
         embed_var_helpme = discord.Embed(description=desc_helpme, color=0x00FF00)
         await interaction.response.send_message(embed=embed_var_helpme)
 
+    async def fetch_trivia_question(self):
+        try:
+            response = requests.get(OTDB_API_URL)
+            response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+            data = response.json()
+
+            if data['response_code'] == 0:
+                result = data['results'][0]
+                question = html.unescape(result['question']) # Decode HTML entities
+                correct_answer = result['correct_answer']
+                incorrect_answers = result['incorrect_answers']
+                return {'question': question, 'correct_answer': correct_answer,
+                        'incorrect_answers': incorrect_answers}
+            else:
+                return None  # Or raise an exception, depending on how you want to handle errors
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching trivia question: {e}")
+            return None
+
+    @app_commands.command(name="trivia", description="Asks a trivia question.")
+    async def trivia(self, interaction: discord.Interaction):
+        user_id = interaction.user.id
+        question_data = await self.fetch_trivia_question()
+
+        if question_data:
+            question = question_data['question']
+            embed = discord.Embed(title="Trivia Question:", description=question,
+                                  color=discord.Color.blue())
+            trivia_view = self.TriviaView(question_data, user_id)  # Pass question data to the view
+            await interaction.response.send_message(embed=embed, view=trivia_view)
+        else:
+            await interaction.response.send_message("Failed to retrieve a trivia question. Please try again later.",
+                                                   ephemeral=True)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # Add the view to the bot so that it will persistent and listen to interactions even when the bot restarts
+        # Check if the persistent view has already been added to prevent adding multiple times
+        if not self.bot.persistent_views_added:
+            #trivia_view = self.TriviaView(question_data)  # Create an instance of TriviaView
+            #self.bot.add_view(trivia_view)  # Now, the instance is being tracked
+            self.bot.persistent_views_added = True
+
+    class TriviaView(discord.ui.View):  # Renamed to TriviaView
+        def __init__(self, question_data=None, user_id=None):
+            super().__init__(timeout=None)
+            if question_data is not None:
+                self.question_data = question_data
+                self.correct_answer = html.unescape(question_data['correct_answer']) # Decode HTML entities
+                incorrect_answers = [html.unescape(answer) for answer in question_data['incorrect_answers']]
+                self.answers = [self.correct_answer] + incorrect_answers
+                random.shuffle(self.answers)
+                self.user_id = user_id
+
+                # Create buttons, assigning correct answer
+                self.a_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[0][:80], custom_id="button_a")
+                self.b_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[1][:80], custom_id="button_b")
+                self.c_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[2][:80], custom_id="button_c")
+                self.d_button = discord.ui.Button(style=discord.ButtonStyle.primary, label=self.answers[3][:80], custom_id="button_d")
+                # Add callback functions to the buttons
+                self.a_button.callback = self.create_callback(self.answers[0])
+                self.b_button.callback = self.create_callback(self.answers[1])
+                self.c_button.callback = self.create_callback(self.answers[2])
+                self.d_button.callback = self.create_callback(self.answers[3])
+
+                # Add buttons to the view
+                self.add_item(self.a_button)
+                self.add_item(self.b_button)
+                self.add_item(self.c_button)
+                self.add_item(self.d_button)
+
+        def create_callback(self, button_answer):
+            async def callback(interaction: discord.Interaction):
+                is_correct = (button_answer == self.correct_answer)
+                if is_correct:
+                    # Update Score
+                    current_score = user_scores.get(self.user_id, 0)  # Get user's score or 0
+                    if current_score == 0:
+                        new_score = 100
+                    else:
+                        new_score = current_score * 2
+
+                    user_scores[self.user_id] = new_score
+
+                    if new_score >= WINNING_SCORE:
+                        description = f"Congratulations! You've reached {WINNING_SCORE} and won the game!"
+                    else:
+                        description = f"Correct! You won {new_score} points!"
+                else:
+                    description = f"Incorrect. The correct answer was {self.correct_answer}"
+                    user_scores[self.user_id] = 0  # Reset score on wrong answer
+
+                # Get updated score
+                updated_score = user_scores.get(self.user_id, 0)
+                embed = discord.Embed(title="Trivia Result", description=f"{description}\nYour score: {updated_score}", color=discord.Color.green() if is_correct else discord.Color.red())
+                #Game Over
+                if updated_score >= WINNING_SCORE:
+                    del user_scores[self.user_id]
+                    view = None
+                else:
+                    view = self
+
+                await interaction.response.edit_message(embed=embed, view=view) # Remove buttons on result
+            return callback
 
 async def setup(bot):
     """
